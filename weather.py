64f0be2b4f7d33a735892d59e721d4a22be4e1d8
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-09 12:24:10 +0300

    using pandas

--- weather.py
+++ weather.py
@@ -1,12 +1,7 @@
-import csv
+import pandas as pd
 class Weather:
     def weather(self):
-        temperatures =[]
-        with open("./weather_data.csv") as weather_file:
-            data = csv.reader(weather_file)
-            for row in data:
-                if row[1] != "temp":
-                    temperatures.append(int(row[1]))
-        print(temperatures)
+        data = pd.read_csv("./weather_data.csv")
+        print(data)
      
 
